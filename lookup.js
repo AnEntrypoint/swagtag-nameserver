84df3842d0608d0f2da8928b603ca4aeb35848a4
lanmower <almagestfraternite@gmail.com>
2022-01-03 22:06:00 +0200

    Update lookup.js

--- lookup.js
+++ lookup.js
@@ -1,79 +1,37 @@
-const Web3 = require("web3");
-const web3 = new Web3("https://api.avax-test.network/ext/bc/C/rpc");
-const NodeCache = require("node-cache");
-const myCache = new NodeCache({ stdTTL: 60 * 60 * 10, checkperiod: 120 });
-const DHT = require("@hyperswarm/dht");
-var base32 = require("hi-base32");
-const node = new DHT();
-const { Packet } = require('dns2');
-
-const ABI = [
-  {
-    constant: true,
-    inputs: [
-      {
-        internalType: "string",
-        name: "_name",
-        type: "string",
-      },
-    ],
-    name: "viewNamesIPAddress",
-    outputs: [
-      {
-        internalType: "string",
-        name: "",
-        type: "string",
-      },
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-];
-
-const ips = {};
-const contract = new web3.eth.Contract(
-  ABI,
-  "0xA237c2A675009a9B3eca8728c8925AF6F4002F29"
-);
-module.exports = async (outname) => {
-  const cache = myCache.get(outname);
-
-  const update = async () => {
-    let address = await contract.methods
-    .viewNamesIPAddress(outname.toLowerCase())
-    .call();
-    if (address.startsWith("tun:")) {
-      let addressout = address.split(':');
-      const hash = addressout.pop();
-      addressout.shift();
-      return {type:Packet.TYPE.CNAME, domain:hash+"."+'entrypoint.ga'};
-    } 
-    if (address.startsWith("ddns:")) {
-      const hash = address.replace("ddns:", "");
-      const publicKey = Buffer.from(base32.decode.asBytes(hash.toUpperCase()));
-      console.log(publicKey.toString("hex"));
-      const noiseSocket = node.connect(publicKey);
-
-      noiseSocket.on("close", function () {
-        console.log("Client closed...");
-      });
-      return await new Promise((done) => {
-        noiseSocket.on("data", function (data) {
-          const res = JSON.parse(data);
-          console.log('ddns found host', res.host);
-          noiseSocket.end();
-          const out = {type:Packet.TYPE.A, address:res.host};
-          myCache.set(outname, out);
-          done(out);
-        });
-      }); 
-    } else {
-      myCache.set(outname, address);
+require('dotenv').config();
+const dns2 = require("dns2");
+const { Packet } = dns2;
+const lookup = require("./lookup.js");
+const pending = {};
+const server = dns2.createServer({
+  udp: true,
+  handle: async (request, send, rinfo) => {
+    const response = Packet.createResponseFromRequest(request);
+    const [question] = request.questions;
+    console.log({question});
+    const { name } = question;
+    let outname = name.split(".");
+    outname.pop();
+    outname.pop();
+    const start = new Date().getTime();
+    let pendingLookup = pending[name];
+    if (!pendingLookup) {
+      pendingLookup = lookup(outname.join(".").toLowerCase(), question.name.toLowerCase());
+      pending[name] = pendingLookup;
     }
-    console.log('normal return', address)
-    return {type:Packet.TYPE.A, address};
-  };
-  if (cache) return cache;
-  return update();
-};
+    let lookedup = await pendingLookup;
+    delete pending[name];
+    for(let answer of lookedup) response.answers.push(answer);
+    console.log({lookedup})
+    send(response);
+  },
+});
+
+server.on("close", () => {
+  console.log("server closed");
+});
+
+server.listen({
+  udp: 53,
+});
+
