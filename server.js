cc49b536c8c96556c3ebfad0160b81610624da3e
lanmower <almagestfraternite@gmail.com>
2022-01-14 07:22:55 +0000

    json configs

--- server.js
+++ server.js
@@ -1,61 +1,75 @@
-require('dotenv').config();
+require("dotenv").config();
 const dns2 = require("dns2");
 const { Packet } = dns2;
 const lookup = require("./lookup.js");
 const pending = {};
 const nets = {
-  'test':{
-    tunnelip:'129.213.57.168',
-    tunnelhost:'test.avax.ga',
-    host:"https://api.avax-test.network/ext/bc/C/rpc",
-    contract:"0x30fd3f22BD652cE1339922D7701b3D35F13c4E46",
+  test: {
+    tunnelip: "129.213.57.168",
+    tunnelhost: "fuji.avax.ga",
+    prefix: "https://domains.fuji.avax.ga/#",
+    host: "https://api.avax-test.network/ext/bc/C/rpc",
+    contract: "0x30fd3f22BD652cE1339922D7701b3D35F13c4E46",
+  }, 
+  fuji: {
+    tunnelip: "129.213.57.168",
+    tunnelhost: "fuji.avax.ga",
+    prefix: "https://domains.fuji.avax.ga/#",
+    host: "https://api.avax-test.network/ext/bc/C/rpc",
+    contract: "0x30fd3f22BD652cE1339922D7701b3D35F13c4E46",
   },
-  'fuji':{
-    tunnelip:'129.213.57.168',
-    tunnelhost:'fuji.avax.ga',
-    host:"https://api.avax-test.network/ext/bc/C/rpc",
-    contract:"0x30fd3f22BD652cE1339922D7701b3D35F13c4E46",
+  avax: {
+    tunnelip: "129.213.57.168",
+    tunnelhost: "avax.ga",
+    prefix: "https://domains.avax.ga/#",
+    host: "https://api.avax.network/ext/bc/C/rpc",
+    contract: "0x1B96Ae207FaB2BAbfE5C8bEc447E60503cD99200",
   },
-  'avax':{
-    tunnelip:'129.213.57.168',
-    tunnelhost:'avax.ga',
-    host:"https://api.avax-test.network/ext/bc/C/rpc",
-    contract:"0x30fd3f22BD652cE1339922D7701b3D35F13c4E46",
-  }
- }
- 
+};
+
 const server = dns2.createServer({
   udp: true,
   handle: async (request, send, rinfo) => {
     const response = Packet.createResponseFromRequest(request);
     const [question] = request.questions;
-    console.log({question});
-    
+    console.log({ question });
+
     const { name } = question;
     let outname = name.split(".");
-    let net = nets['avax'];
+    let net = nets["avax"];
 
-    if(outname.length > 2 && Object.keys(nets).includes(outname[outname.length-3])) {
-      net = nets[outname[outname.length-3]];
+    if (
+      outname.length > 2 &&
+      Object.keys(nets).includes(outname[outname.length - 3])
+    ) {
+      net = nets[outname[outname.length - 3]];
       outname.pop();
     }
-    if(outname.length > 1 && Object.keys(nets).includes(outname[outname.length-2])) {
-      net = nets[outname[outname.length-2]];
+    if (
+      outname.length > 1 &&
+      Object.keys(nets).includes(outname[outname.length - 2])
+    ) {
+      net = nets[outname[outname.length - 2]];
     }
-  
+
     outname.pop();
     outname.pop();
     let pendingLookup = pending[name];
     if (!pendingLookup) {
-      pendingLookup = lookup(outname.join(".").toLowerCase(), question.name.toLowerCase(), net.host, net.contract, net.tunnelhost, net.tunnelip);
+      pendingLookup = lookup(
+        outname.join(".").toLowerCase(),
+        question,
+        net.host,
+        net
+      );
       pending[name] = pendingLookup;
     }
     let lookedup = await pendingLookup;
     delete pending[name];
-    console.log(lookedup);
-    if(!lookedup) return send(response);
-    for(let answer of lookedup) response.answers.push(answer);
-    console.log({lookedup})
+    if (!lookedup) return send(response);
+
+    for (let answer of lookedup) response.answers.push(answer);
+    console.log(response.questions, response.answers);
     send(response);
   },
 });
@@ -67,4 +81,3 @@ server.on("close", () => {
 server.listen({
   udp: 53,
 });
-
